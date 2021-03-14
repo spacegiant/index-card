3bcb62b5c0d01ef63fff0c63fc4125ac095eb8e9
spacegiant <599280+spacegiant@users.noreply.github.com>
2021-03-14 20:47:24 +0000

    Added webpack, eslint and prettier

---
+++ webpack.config.js
@@ -0,0 +1,60 @@
+const path = require("path");
+const webpack = require("webpack");
+
+/*
+ * We've enabled MiniCssExtractPlugin for you. This allows your app to
+ * use css modules that will be moved into a separate CSS file instead of inside
+ * one of your module entries!
+ *
+ * https://github.com/webpack-contrib/mini-css-extract-plugin
+ *
+ */
+
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+
+  output: {
+    path: path.resolve(__dirname, "scripts"),
+  },
+
+  plugins: [
+    new webpack.ProgressPlugin(),
+    new MiniCssExtractPlugin({ filename: "main.[contenthash].css" }),
+  ],
+
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        include: [],
+        loader: "babel-loader",
+      },
+      {
+        test: /.(sa|sc|c)ss$/,
+
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+          },
+          {
+            loader: "css-loader",
+
+            options: {
+              sourceMap: true,
+            },
+          },
+          {
+            loader: "sass-loader",
+
+            options: {
+              sourceMap: true,
+            },
+          },
+        ],
+      },
+    ],
+  },
+};
